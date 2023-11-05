76ace7767b1269186fd3b9cad1a8f8adbaa61e58
Elohor Godwin <elohorgodwin4298@gmail.com>
2023-11-05 04:22:59 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         My name is Elohor, this is a dev feature.
         </p>
         <a
           className="App-link"
